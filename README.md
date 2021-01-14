cf61db71d6466dd5124739fc953a8f06f80fdb97
Sanders Gutierrez <ing.sanders@gmail.com>
2021-01-14 05:25:52 -0300

    Prueba de funcionamiento y agrego cambios sutiles.
    - Soporte para prettier para formatear el codigo.
    - Control de variables de ambiente multiplataforma con `cross-env`.
    - Soporte para EditorConfig
    - Elimino .env y muevo variables a .env.example
    - Muevo archivos del proyecto de server/ a src/
    - Archivo LICENSE
    - Archivo README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# SOSHACE-RBAC
+
+Ejemplo de como crear una api con un sistema de control de acceso basados en roles.
+
+## Requerimientos
+
+-   Node v10+
+-   MongoDB v4.x
+
+### Configurar ambiente
+
+1. Copiar .env.example a .env
+
+```
+cp .env.example .env
+```
+
+2. Instalar dependencias del proyecto
+
+```
+npm install
+```
+
+## Ejecutar la aplicacion
+
+```
+# Development
+npm run start:dev
+
+# Production
+npm start
+```
---
+++ src/models/userModel.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: 'basic',
+        enum: ['basic', 'supervisor', 'admin'],
+    },
+    accessToken: {
+        type: String,
+    },
+})
+
+const User = mongoose.model('user', UserSchema)
+
+module.exports = User
